223f3c7c352b043bf35979df0f05ff71cdc1a4be
moiid212834 <32700511+moiid212834@users.noreply.github.com>
2020-11-07 18:01:03 +0500

    deployed

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Head from 'next/head'
+import gitead from 'next/head'
 
 export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Create First Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
